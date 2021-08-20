729bfd68a33a91b39a28b965c43d7989f420edf2
gst122 <gst200521@163.com>
2021-08-20 19:57:01 +0800

    master更新操作

---
+++ src/test/java/org/example/Test01.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
+        System.out.println("test04...");
+        System.out.println("master->test04...");
+
+    }
+}
